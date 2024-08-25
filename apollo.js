327bfdf9755741f73348fba6089e143f89a5b517
Debasis208 <debasismohanty209@gmail.com>
2024-08-25 18:56:40 +0530

    Add files via upload

---
+++ apollo.js
@@ -0,0 +1,8 @@
+const menubar = document.querySelector(".menu_bar_section");
+const menulogo = document.querySelector(".menu_section");
+
+menulogo.addEventListener("click",()=>{
+    menulogo.classList.toggle("menubar")
+}
+
+)
