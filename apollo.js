ca1cf56d493c52b1fd4f699c0cb911376c1e8778
Debasis208 <debasismohanty209@gmail.com>
2024-09-19 11:30:20 +0530

    Delete apollo.js

--- apollo.js
+++
@@ -1,8 +0,0 @@
-const menubar = document.querySelector(".menu_bar_section");
-const menulogo = document.querySelector(".menu_section");
-
-menulogo.addEventListener("click",()=>{
-    menulogo.classList.toggle("menubar")
-}
-
-)
