4cada37d8e8d4ed3a6b84b8bcb897ffded10783d
Debasis208 <debasismohanty209@gmail.com>
2024-09-19 11:35:52 +0530

    Update apollo.js

--- apollo.js
+++ apollo.js
@@ -1,44 +1,4 @@
 
-// const menubar = document.querySelector(".menu_bar_section");
-// const menulogo = document.querySelector(".menu_section");
-// const cancelButton = document.querySelector(".cancel_button");
-
-// function addSection(value){
-//     menubar.style.opacity = value;
-// }
-
-
-// menulogo.addEventListener("click", () =>{
-//     addSection("1");
-// });
-
-// cancelButton.addEventListener("click", () => {
-//     addSection("0");
-// })
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Get references to the elements
-//     const menuButton = document.querySelector('.menu_section');
-//     const menuBar = document.querySelector('#menu_bar_section');
-//     const cancelButton = document.querySelector('.cancel_button');
-
-//     // Ensure both elements exist before adding event listeners
-//     if (menuButton && menuBar && cancelButton) {
-//         // Add click event listener to the menu button
-//         menuButton.addEventListener('click', function() {
-//             // Toggle visibility of the menu bar section
-//             menuBar.classList.toggle('hidden');
-//         });
-
-//         // Add click event listener to the cancel button
-//         cancelButton.addEventListener('click', function() {
-//             // Hide the menu bar section
-//             menuBar.classList.add('hidden');
-//         });
-//     }
-// });
 
 document.addEventListener('DOMContentLoaded', function() {
     // Get references to the elements
